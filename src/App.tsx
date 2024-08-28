3d1569db5e526c24509731b30cf8b3f4d977dbfe
naoki <n.globe.us@gmail.com>
2024-08-28 10:49:30 +0900

    papaparseに置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,10 @@ import Images from './App/Images'
 
 import Tabbar from './App/Tabbar'
 import config from "./config.json";
-import csvParser from 'csv-parse'
+import Papa from 'papaparse'
 
 // You can see config.json after running `npm start` or `npm run build`
 // import config from './config.json'
-
 
 const sortShopList = async (shopList: Pwamap.ShopData[]) => {
 
@@ -25,62 +24,45 @@ const sortShopList = async (shopList: Pwamap.ShopData[]) => {
 
 }
 
-const zen2han = (str: string) => {
-  return str.replace(/[！-～]/g, function(s: string) {
-    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-  }).replace(/　/g, ' ');
-}
-
 const App = () => {
   const [shopList, setShopList] = React.useState<Pwamap.ShopData[]>([])
 
   React.useEffect(() => {
-    fetch(`${encodeURIComponent(config.data_url)}`)
+    fetch(config.data_url)
     .then((response) => {
       return response.ok ? response.text() : Promise.reject(response.status);
     })
     .then((data) => {
-      csvParser(data, async (error, data) => {
-        if (error) {
-          console.log(error)
-          setShopList([])
-          return
+
+      Papa.parse(data, {
+        header: true,
+        complete: (results) => {
+          const features = results.data
+
+          const nextShopList: Pwamap.ShopData[] = []
+          for (let i = 0; i < features.length; i++) {
+            const feature = features[i] as Pwamap.ShopData
+            if (!feature['緯度'] || !feature['経度'] || !feature['スポット名']) {
+              continue;
+            }
+            if (!feature['緯度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
+              continue
+            }
+            if (!feature['経度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
+              continue
+            }
+            const shop = {
+              // @ts-ignore
+              index: i,
+              ...feature
+            }
+
+            nextShopList.push(shop)
+          }
+          sortShopList(nextShopList).then((sortedShopList) => {
+            setShopList(sortedShopList)
+          })
         }
-
-        const [header, ...records] = data;
-
-        const features = records.map((record: string) => {
-          const properties = header.reduce((prev: any, column: any) => {
-            const value = record[header.indexOf(column)];
-            prev[column] = zen2han(value);
-            return prev;
-          }, {});
-
-          return properties;
-        });
-
-        const nextShopList: Pwamap.ShopData[] = []
-        for (let i = 0; i < features.length; i++) {
-          const feature = features[i] as Pwamap.ShopData
-          if (!feature['緯度'] || !feature['経度'] || !feature['スポット名']) {
-            continue;
-          }
-          if (!feature['緯度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
-            continue
-          }
-          if (!feature['経度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
-            continue
-          }
-          const shop = {
-            index: i,
-            ...feature
-          }
-
-          nextShopList.push(shop)
-        }
-        sortShopList(nextShopList).then((sortedShopList) => {
-          setShopList(sortedShopList)
-        })
       });
     });
   }, [])
